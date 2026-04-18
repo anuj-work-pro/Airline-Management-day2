095567eb067ad14fe35e3e9235fd76c80392c6c9
Anuj-works <vasutomar0001@gmail.com>
2026-04-18 14:21:06 +0530

    new changes

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,41 @@
-# Copyright (c) 2026, anuj and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+import random
+import string
 
 class AirplaneTicket(Document):
-	pass
+    def validate(self):
+        self.calculate_total()
+        self.remove_duplicates()
+
+    def calculate_total(self):
+        total = self.flight_price or 0
+
+        for addon in self.add_ons:
+            total += addon.amount or 0
+
+        self.total_price = total
+
+    def remove_duplicates(self):
+        seen = []
+        unique_rows = []
+
+        for row in self.add_ons:
+            if row.item not in seen:
+                seen.append(row.item)
+                unique_rows.append(row)
+
+        self.set("add_ons", unique_rows)
+
+    def before_submit(self):
+        if self.status != "Boarded":
+           frappe.throw("Ticket can be submitted only when status is Boarded")
+
+    def before_insert(self):
+        number = random.randint(1, 99)
+        letter = random.choice(["A", "B", "C", "D", "E"])
+        self.seat = f"{number}{letter}"
+
+
+    def on_submit(self):
+        self.status = "Completed"
